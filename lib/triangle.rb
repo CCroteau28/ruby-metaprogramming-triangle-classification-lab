f4f9aa8ab4951fa9cdb68f9f11d32ac5c98a0c76
CCroteau28 <ccroteau28@gmail.com>
2021-03-24 08:50:02 -0600

    commit tree 26bb9db92a1997d196d9124f87d5f1f1710172d4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Triangle
+  attr_accessor :length, :width, :height
   # write code here
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    if length <= 0 || width <= 0 || height <= 0
+      raise TriangleError
+    end
+    if length >= (width + height) || width >= (height + length) || height >= (length + width)
+      raise TriangleError
+    end
+    if length == width && width == height && height == length
+      return :equilateral
+    elsif length == height || length == width || height == width
+      return :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+# new_triangle = Triangle.new(10, 10, 20)
+# binding.pry
